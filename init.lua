a4e5849d383a20977427fc1f9edd0d69c2d943d8
VOID65 <vladimzor15@gmail.com>
2025-10-05 00:57:25 +0000

    Meow

---
+++ init.lua
@@ -0,0 +1,7 @@
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/VOID65/ARCHER/refs/heads/main/WindUI.lua"))()
+
+local UI = WindUI:CreateWindow({
+    Title = "Пример интерфейса",
+    Theme = "Dark",
+    CanDraggable = true,
+})
